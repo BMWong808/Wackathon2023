9f2279681078803750e79c22f48da826287327c1
charlotte-sk <118553231+charlotte-sk@users.noreply.github.com>
2023-03-11 14:03:31 +0000

    Create database.py
    Initial commit of database.py

---
+++ database.py
@@ -0,0 +1,22 @@
+import json
+
+class Item:
+    def __init__(self, name, code, stock, price):
+        self.name = name
+        self.code = code
+        self.price = price
+        self.stock = stock
+
+
+
+
+def add_item_to_db(item):
+    pass
+
+
+def delete_item_from_db(code):
+    pass
+
+
+def get_all_items():
+    pass
